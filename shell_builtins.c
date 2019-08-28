dc59a173f58dc32aaac640482b0bcdfbe2ce776b
Faizan <711@holbertonschool.com>
2019-08-28 21:33:14 +0000

    Adding built in functionality.

---
+++ shell_builtins.c
@@ -0,0 +1,63 @@
+#include "shell.h"
+
+/**
+ * exit_hsh - Exits from simple shell
+ * @cmd: Inputted command
+ *
+ * Return: 0
+ */
+int exit_hsh(char **cmd)
+{
+	if (*cmd)
+		exit(1);
+	return (0);
+}
+
+
+/**
+ * print_env - Print the environment
+ * @cmd: cmd pointer
+ *
+ * Return: 0
+ */
+
+int print_env(char **cmd)
+{
+	int x;
+
+	if (*cmd)
+	{
+
+		for (x = 0; environ[x]; x++)
+		{
+			write(1, environ[x], _strlen(environ[x]));
+			write(1, "\n", 1);
+		}
+	}
+
+	return (0);
+}
+
+/**
+ * call_built - Check for builtin, then calls
+ * @cmbn: full dir
+ * @cmd: cmd input
+ *
+ * Return: path to builtin or process from dir
+ */
+
+int call_built(char *cmbn, char **cmd)
+{
+	int x;
+	char *arr[] = {"exit", "env", NULL};
+
+	typedef int (*Builtins)(char **);
+	Builtins functions[] = {&exit_hsh, &print_env};
+
+	for (x = 0; arr[x] != NULL; x++)
+	{
+		if (_strcmp(arr[x], cmd[0]) == 0)
+			return (functions[x](cmd));
+	}
+	return (exec_cmd(cmbn, cmd));
+}
